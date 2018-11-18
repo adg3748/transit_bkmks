8d4e503b68649d98e2140bfe6f99b20ef67e37e3
LilPacy <revivedtomorrow@gmail.com>
2018-11-18 20:58:59 +0700

    get transit-bkmks folder

--- background.js
+++ background.js
@@ -3,11 +3,38 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
   });
 });
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.bookmarks.getTree(function(roots){
 //    var url = roots[0].children[0].children[0].url
 //      console.log(url);
 //      chrome.tabs.create({'url':url}, function(tab){console.log(tab);});
-      console.log(roots);
+    console.log(roots);
+    console.log(roots[0].children[0].children);
+    var bkmk_br = roots[0].children[0].children // Array
+    bkmk_br.some(function(bkmk){
+      if(bkmk.title=='transit-bkmks'){
+        var folder = bkmk;
+        console.log('I found you!', folder);
+        return true;
+      }
+    });
   });
 });
+
+//Array(1)
+//0:
+//children: Array(2)
+//0:
+//children: Array(125)
+//[0 … 99]
+//0:
+//children: Array(0)
+//length: 0
+//__proto__: Array(0)
+//dateAdded: 1542548448670
+//dateGroupModified: 1542548448670
+//id: "1288"
+//index: 0
+//parentId: "1"
+//title: "transit-bkmks"
