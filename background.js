337b23a637b81e2322999fbc3b297898e4834178
LilPacy <revivedtomorrow@gmail.com>
2018-11-18 20:40:03 +0700

    add the function making a new folder when installed

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.bookmarks.create({ 'parentId': '1', 'index': 0, 'title': 'transit-bkmks' });
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+  });
+});
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.bookmarks.getTree(function(roots){
-    var url = roots[0].children[0].children[0].url
-    //var url = roots[0].children[0].children[0].url.match(/(http.+)(?=\')/)[0];
-      console.log(url);
-      chrome.tabs.create({'url':url}, function(tab){console.log(tab);});
+//    var url = roots[0].children[0].children[0].url
+//      console.log(url);
+//      chrome.tabs.create({'url':url}, function(tab){console.log(tab);});
       console.log(roots);
   });
 });
